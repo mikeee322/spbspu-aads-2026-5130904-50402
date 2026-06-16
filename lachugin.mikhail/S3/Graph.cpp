ddc1495be17f5a3ece1efd8ba0c77556f6cd3ccb
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-17 01:16:05 +0300

    start develop Graph

--- lachugin.mikhail/S3/Graph.cpp
+++ lachugin.mikhail/S3/Graph.cpp
@@ -3,8 +3,63 @@
 
 namespace lachugin {
 
-  Graph::Graph()
+  Graph::Graph():
+    edges_(50, 5, 20)
   {}
+
+  bool Graph::hasEdge(const std::string &from, const std::string &to) const
+  {
+    Edge edge{from, to};
+    return edges_.has(edge);
+  }
+
+  void lachugin::Graph::bind(
+    const std::string& from,
+    const std::string& to,
+    size_t weight)
+  {
+    addVertex(from);
+    addVertex(to);
+
+    Edge edge;
+    edge.from = from;
+    edge.to = to;
+
+    if (!edges_.has(edge))
+    {
+      Weights weights;
+      weights.pushBack(weight);
+      edges_.add(edge, weights);
+      return;
+    }
+
+    edges_.get(edge).pushBack(weight);
+  }
+  void Graph::cut(const std::string& from,const std::string& to, size_t weight)
+  {
+    Edge edge;
+    edge.from = from;
+    edge.to = to;
+
+    if (!edges_.has(edge))
+    {
+      throw std::logic_error("Edge not found");
+    }
+    Weights& weights = edges_.get(edge);
+    for (auto it = weights.begin(); it != weights.end(); ++it)
+    {
+      if (*it == weight)
+      {
+        weights.erase(it);
+        if (weights.empty())
+        {
+          edges_.drop(edge);
+        }
+        return;
+      }
+    }
+    throw std::logic_error("Weight not found");
+  }
 
   bool Graph::hasVertex(const std::string &name) const
   {
--- lachugin.mikhail/S3/Graph.hpp
+++ lachugin.mikhail/S3/Graph.hpp
@@ -1,20 +1,34 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
 #include "../common/list.hpp"
+#include "../common/itters.hpp"
 #include "HashTable.hpp"
+#include "Edge.hpp"
 
 namespace lachugin
 {
   class Graph
   {
   public:
+    using Weights = List<size_t>;
     Graph();
+
     void addVertex(const std::string& name);
     bool hasVertex(const std::string& name) const;
 
-  private:
-    List < std::string > vertexes_;
+    void bind(const std::string& from, const std::string& to, size_t weight);
+    bool hasEdge(const std::string& from, const std::string& to) const;
+    void cut(const std::string& from, const std::string& to, size_t weight);
 
+  private:
+    List<std::string> vertexes_;
+
+    HashTable<
+        Edge,
+        Weights,
+        EdgeHash,
+        EdgeEqual
+    > edges_;
   };
 
 
