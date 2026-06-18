72d40e16e0f195ab12ebfa68637ac3ed3a2cc218
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-19 01:53:01 +0300

    add sort for list

--- lachugin.mikhail/S3/commands.cpp
+++ lachugin.mikhail/S3/commands.cpp
@@ -3,6 +3,32 @@
 
 namespace lachugin
 {
+  template< class T >
+  void sortList(List< T >& list)
+  {
+    if (list.size() < 2)
+    {
+      return;
+    }
+    for (auto it1 = list.begin(); it1 != list.end(); ++it1)
+    {
+      auto minIt = it1;
+      for (auto it2 = it1; it2 != list.end(); ++it2)
+      {
+        if (*it2 < *minIt)
+        {
+          minIt = it2;
+        }
+      }
+      if (minIt != it1)
+      {
+        T tmp = *it1;
+        *it1 = *minIt;
+        *minIt = tmp;
+      }
+    }
+  }
+
   void cmdGraphs(std::istream&, std::ostream& out, GraphStorage& storage)
   {
     auto& graphs = storage.getGraphs();
