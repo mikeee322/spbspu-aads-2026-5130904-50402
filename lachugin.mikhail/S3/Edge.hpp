f49776cafae97dd499a2b5083e493edc06d2716e
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-19 05:30:38 +0300

    add SHA-1

--- lachugin.mikhail/S3/Edge.hpp
+++ lachugin.mikhail/S3/Edge.hpp
@@ -1,6 +1,7 @@
 #ifndef EDGE_HPP
 #define EDGE_HPP
 #include <cstddef>
+#include <boost/hash2/hash_append.hpp>
 #include <string>
 namespace lachugin
 {
@@ -19,6 +20,18 @@ namespace lachugin
   {
     size_t operator()(const Edge& e) const;
   };
+
+  template<class Provider, class Hash, class Flavor>
+  void tag_invoke(
+  boost::hash2::hash_append_tag,
+  Provider,
+  Hash& h,
+  Flavor const& f,
+  Edge const* e)
+  {
+    boost::hash2::hash_append(h, f, e->from);
+    boost::hash2::hash_append(h, f, e->to);
+  }
 }
 #endif
 
