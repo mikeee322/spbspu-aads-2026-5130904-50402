cbb70d191bfefe4324b82efeef8f43f91f21b222
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-20 22:43:54 +0300

    make rule5

--- lachugin.mikhail/F0/CuckooIter.hpp
+++ lachugin.mikhail/F0/CuckooIter.hpp
@@ -9,7 +9,6 @@ namespace lachugin
   class CuckooIter
   {
   public:
-    using Iterator = CuckooIter< Key, Value, Hash1, Hash2, Equal >;
     friend class CuckooHashTable< Key, Value, Hash1, Hash2, Equal >;
     std::pair< Key, Value >& operator*() const;
     std::pair< Key, Value> * operator->() const;
@@ -29,7 +28,6 @@ namespace lachugin
   class CuckooConstIter
   {
   public:
-    using ConstIterator = CuckooConstIter< Key, Value, Hash1, Hash2, Equal >;
     friend class CuckooHashTable<Key, Value, Hash1, Hash2, Equal>;
     std::pair< Key, Value >& operator*() const;
     std::pair< Key, Value> * operator->() const;
@@ -46,6 +44,45 @@ namespace lachugin
   };
 
 
+  template< class Key, class Value, class Hash1, class Hash2, class Equal >
+  CuckooIter< Key, Value, Hash1, Hash2, Equal >::CuckooIter(
+    CuckooHashTable< Key, Value, Hash1, Hash2, Equal >* table, bool secondTable, size_t pos):
+  table_(table),
+  secondTable_(secondTable),
+  pos_(pos)
+  {}
+
+  template< class Key, class Value, class Hash1, class Hash2, class Equal >
+  std::pair< Key, Value >& CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator*() const
+  {
+    if (!secondTable_)
+    {
+      return table_->table1_[pos_].data;
+    }
+    return table_->table2_[pos_].data;
+  }
+
+  template< class Key, class Value, class Hash1, class Hash2,class Equal >
+  std::pair< Key, Value >* CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator->() const
+  {
+    if (!secondTable_)
+    {
+      return &table_->table1_[pos_].data;
+    }
+    return &table_->table2_[pos_].data;
+  }
+
+  template< class Key, class Value, class Hash1, class Hash2, class Equal >
+  bool CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator==(const CuckooIter& other) const
+  {
+    return table_ == other.table_ && secondTable_ == other.secondTable_ && pos_ == other.pos_;
+  }
+
+  template< class Key, class Value, class Hash1, class Hash2, class Equal >
+  bool CuckooIter< Key, Value, Hash1, Hash2, Equal >::operator!=(const CuckooIter& other) const
+  {
+    return !(*this == other);
+  }
 
 
 }
