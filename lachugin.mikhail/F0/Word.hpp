7f2cdb070138d20a8be816610d6bf2e418b33565
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-21 00:17:36 +0300

    add more methods

--- lachugin.mikhail/F0/Word.hpp
+++ lachugin.mikhail/F0/Word.hpp
@@ -9,7 +9,22 @@ namespace lachugin
 {
   class Word
   {
-    List <std::string > translations_;
+  public:
+    Word();
+    Word(const std::string& translation);
+
+    void addTranslation(const std::string& translation);
+    bool hasTranslation(const std::string& translation) const;
+    List< std::string >& getTranslations();
+    const List< std::string >& getTranslations() const;
+    void setInterpretation(const std::string& interpretation);
+    void removeInterpretation();
+    bool hasInterpretation() const;
+    const std::string& getInterpretation() const;
+    void setPartOfSpeech(const std::string& partOfSpeech);
+    const std::string& getPartOfSpeech() const;
+  private:
+    List< std::string > translations_;
     std::string interpretation_;
     std::string partOfSpeech_;
   };
