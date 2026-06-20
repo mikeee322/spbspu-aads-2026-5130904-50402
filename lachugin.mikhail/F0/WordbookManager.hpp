6619e9fe6b610f73933cdee6d15b2f97f311d1d9
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-21 02:56:29 +0300

    start worfbook manager

---
+++ lachugin.mikhail/F0/WordbookManager.hpp
@@ -0,0 +1,27 @@
+#ifndef WORDBOOKMANAGER_HPP
+#define WORDBOOKMANAGER_HPP
+
+#include "CuckooHashTable.hpp"
+#include "Wordbook.hpp"
+namespace lachugin
+{
+  class WordbookManager
+  {
+  public:
+    void createWordbook(const std::string& name);
+    void deleteWordbook(const std::string& name);
+    bool hasWordbook(const std::string& name) const;
+    Wordbook& getWordbook(const std::string& name);
+
+    const Wordbook& getWordbook(const std::string& name) const;
+    void merge(const std::string& frst,const std::string& scn, const std::string& newName);
+  private:
+    CuckooHashTable< std::string, Wordbook, DefaultHash1< std::string >, DefaultHash2< std::string >,
+      std::equal_to< std::string > > wordbooks_;
+
+  };
+
+
+}
+
+#endif
