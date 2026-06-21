bfd0215701e1c277f0cf7cf08195aff5c7001a19
Михаил Лачугин <mikee@MacBook-Air-Mihail.local>
2026-06-21 04:02:53 +0300

    add tests for merge

--- lachugin.mikhail/F0/test-wordbookmanager.cpp
+++ lachugin.mikhail/F0/test-wordbookmanager.cpp
@@ -37,3 +37,24 @@ BOOST_AUTO_TEST_CASE(wordbookmanager_get_wordbook_test)
   BOOST_TEST(manager.getWordbook("dict").getName() == "dict");
 }
 
+BOOST_AUTO_TEST_CASE(wordbookmanager_merge_test)
+{
+  WordbookManager manager;
+  manager.createWordbook("first");
+  manager.createWordbook("second");
+
+  manager.getWordbook("first").addWord("watermelon", "арбуз");
+  manager.getWordbook("second").addWord("orange", "апельсин");
+  manager.merge("first", "second", "merged");
+
+  BOOST_TEST(manager.hasWordbook("merged"));
+  BOOST_TEST(manager.getWordbook("merged").hasWord("watermelon"));
+  BOOST_TEST(manager.getWordbook("merged").hasWord("orange"));
+}
+
+BOOST_AUTO_TEST_CASE(wordbookmanager_merge_missing_wordbook_test)
+{
+  WordbookManager manager;
+  manager.createWordbook("first");
+  BOOST_CHECK_THROW(manager.merge( "first", "second", "merged"),std::logic_error);
+}
